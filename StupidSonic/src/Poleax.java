7340dde967a5a60a243621360c32c6823128bcc3
hunggaxx <vuthehung17@gmail.com>
2016-03-19 04:24:20 +0700

    Turn3 - Team2 Complete

--- StupidSonic/src/Poleax.java
+++ StupidSonic/src/Poleax.java
@@ -6,32 +6,37 @@ import java.util.Random;
 
 public class Poleax extends Enemy{
     Random random = new Random();
+    Animation dungduaAnimation;
     public Poleax() {
 
-        try {
-            setSprite(ImageIO.read(new File(Resources.poleax2)));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            setSprite(ImageIO.read(new File(Resources.poleax2)));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        dungduaAnimation = new Animation(0,4,"Resources/Poleax/Poleax",".png",90);
         setPositionY(-50);
         setPositionX(3000);
         setSpeed(6);
     }
 
     public boolean checkCollistion() {
-        Rectangle rectPoleax = new Rectangle(this.getPositionX(), this.getPositionY()
-                , 160
-                , 340);
+        Rectangle rect1 = new Rectangle(this.getPositionX()+82, this.getPositionY()+54
+                , 13
+                , 242);
+        Rectangle rect2 = new Rectangle(this.getPositionX(), this.getPositionY()+310
+                , 360
+                , 50);
+
         Rectangle rectSonic =
                 new Rectangle(SonicManager.getInstance().getStupidSonic().getPositionX()
                         , SonicManager.getInstance().getStupidSonic().getPositionY()
                         ,60
                         ,70);
-        if (rectPoleax.intersects(rectSonic)) {
+        if (rect1.intersects(rectSonic)||rect2.intersects(rectSonic)) {
             return true;
         }else return false;
     }
-
     private void move() {
         setPositionX(getPositionX() - getSpeed());
         if (getPositionX() <= -300) {
@@ -58,9 +63,10 @@ public class Poleax extends Enemy{
 
         }
     }
+
     @Override
     public void update() {
-        if(this.checkCollistion() && Sonic.isSuper==false){
+        if(this.checkCollistion()){
             GameWindow.isLose = true;
         }
         this.move();
@@ -68,6 +74,7 @@ public class Poleax extends Enemy{
 
     @Override
     public void draw(Graphics g) {
-        g.drawImage(getSprite(), getPositionX(), getPositionY(), null);
+//        g.drawImage(getSprite(), getPositionX(), getPositionY(), null);
+        dungduaAnimation.draw(g,getPositionX(),getPositionY());
     }
 }
